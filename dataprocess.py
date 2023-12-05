04b61863f3e535f7191def13a86ca32856d560db
hejinhua1 <2584944133@qq.com>
2023-12-05 22:55:12 +0800

    processed data

--- dataprocess.py
+++ dataprocess.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 import numpy as np
 
 # 读取CSV文件
@@ -11,16 +11,23 @@ forecast_data = forecast_data.drop(forecast_data.columns[0], axis=1)
 real_data = real_data.drop(real_data.columns[0], axis=1)
 # 合并两个DataFrame
 merged_df = pd.merge(forecast_data, real_data, on='dtime', how='outer')
-merged_df = merged_df.drop(merged_df.columns[0], axis=1)
+feature_df = merged_df.drop(merged_df.columns[0], axis=1)
 # 用均值填充缺失值
-filled_df = merged_df.fillna(merged_df.mean())
+feature_df = feature_df.fillna(feature_df.mean())
 # 提取特征数据
 
-features = filled_df.values
+features = feature_df.values
 
 # 使用MinMaxScaler进行归一化处理
-scaler = MinMaxScaler()
-normalized_data = scaler.fit_transform(features)
+minmaxscaler = MinMaxScaler()
+standardscaler = StandardScaler()
+minmax_data = minmaxscaler.fit_transform(features)
+standard_data = standardscaler.fit_transform(features)
+
 
 # 保存归一化后的数据为npy格式
-np.save('normalized_data.npy', normalized_data)
+save_path = 'E:/HJHCloud/Seafile/startup/GoldWindPower/data'
+np.save(save_path + 'minmax_data.npy', minmax_data)
+np.save(save_path + 'std_data.npy', standard_data)
+# 保存merged_df为CSV文件
+merged_df.to_csv(save_path + 'merged_data.csv', index=False)
--- staticConst.py
+++ staticConst.py
@@ -6,12 +6,30 @@ class AtmosphericData:
         self.data_max = np.array([9.87746652e+02,1.00000000e+02, 3.35194468e+01, 2.72610000e+01,
                                   3.60000000e+02, 1.52400000e+01, 4.38600006e+01, 8.45808438e+04])
         self.data_min = np.array([949.877, 21.834, 1.24, -12.17596966, 0., 0., 0., 0.])
-        self.data_mean = np.array()
-        self.data_std = np.array()
+        self.data_mean = np.array([9.71854897e+02, 8.06169909e+01, 2.10091182e+01, 1.72234666e+01,
+                                   1.22577295e+02, 3.62705178e+00, 5.02223864e+00, 2.25289376e+04])
+        self.data_var = np.array([3.50831492e+01, 2.55231449e+02, 3.73100321e+01, 5.00286586e+01,
+                                  6.44395647e+03, 3.34081774e+00, 7.78148638e+00, 5.36015184e+08])
+        self.data_std = np.sqrt(self.data_var)
 
-        self.z_std_dict = {}
+        self.max_dict = {'power': self.data_max[7], 'rwind': self.data_max[6], 'fwind': self.data_max[5]}
+        self.min_dict = {'power': self.data_min[7], 'rwind': self.data_min[6], 'fwind': self.data_min[5]}
+        self.mean_dict = {'power': self.data_mean[7], 'rwind': self.data_mean[6], 'fwind': self.data_mean[5]}
+        self.std_dict = {'power': self.data_std[7], 'rwind': self.data_std[6], 'fwind': self.data_std[5]}
+
         self.Typhoon = {
-
+            '2021': [
+                ('2021-06-03', '2021-06-05'),
+                ('2021-08-04', '2021-08-07'),
+                ('2021-09-09', '2021-09-12'),
+                ('2021-10-11', '2021-10-12')
+            ],
+            '2022': [
+                ('2022-06-03', '2022-06-05'),
+                ('2022-08-04', '2022-08-07'),
+                ('2022-09-09', '2022-09-12'),
+                ('2022-10-11', '2022-10-12')
+            ]
         }
 
 
@@ -22,5 +40,4 @@ if __name__ == '__main__':
 
     # 现在你可以访问数据和字典
     print(atmospheric_data.data_max)
-    print(atmospheric_data.z_std_dict['50'])
     print('finished')
